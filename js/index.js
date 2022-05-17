36acea5c9b31e28b0634d94d8617c80686de0ee6
fuzail <fuzailafsar92@gmail.com>
2022-05-17 22:38:36 +0500

    chapter 1 & 2

---
+++ js/index.js
@@ -0,0 +1,62 @@
+/**
+ * CHAPTER 1
+ */
+
+// QUESTION 1
+// alert("Hello world");
+
+// QUESTION 2
+// alert("Error! Please enter a valid password.");
+
+// QUESTION 3
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+// QUESTION 4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+/**
+ * CHAPTER 2
+ */
+
+// QUESTION 1
+// var username;
+
+// QUESTION 2
+// var myName = "Fuzail Afsar";
+
+// QUESTION 3
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// QUESTION 4
+// var name = "Jhone Doe";
+// var age = "15 years old";
+// var course = "Certified Mobile Application Development";
+// alert(name);
+// alert(age);
+// alert(course);
+
+// QUESTION 5
+// var pizzas = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizzas);
+
+// QUESTION 6
+// var email;
+// email = "fuzailafsar92@gmail.com";
+// alert("My email address is " + email);
+
+// QUESTION 7
+// var book;
+// book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+// QUESTION 8
+// var para = document.createElement("p");
+// para.innerHTML = "Yah! I can write HTML content through JavaScript";
+// document.getElementsByTagName("body")[0].append(para);
+
+// QUESTION 9
+// var pattern = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(pattern);
